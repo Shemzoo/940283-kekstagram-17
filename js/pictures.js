5c7422b977b78be4bb952e9582d3cbf17ad35f8f
Shemzoo <46892255+Shemzoo@users.noreply.github.com>
2019-07-19 12:16:39 +0300

    Picture object is now encapsulated in miniature.js module

--- js/pictures.js
+++ js/pictures.js
@@ -2,8 +2,6 @@
 
 (function () {
 
-  var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
-  var picturesContainer = document.querySelector('.pictures');
   var pictures = [];
 
   var createPictures = function (picturesData) {
@@ -39,41 +37,6 @@
     window.filter(picturesData);
   };
 
-
-  var Picture = function (data) {
-    this.data = data;
-    this.url = data.url;
-    this.likes = data.likes;
-    this.description = data.description;
-    this.comments = data.comments;
-
-    this.pictureElement = pictureTemplate.cloneNode(true);
-  };
-
-  Picture.prototype.create = function () {
-    var img = this.pictureElement.querySelector('.picture__img');
-    var likes = this.pictureElement.querySelector('.picture__likes');
-    var comments = this.pictureElement.querySelector('.picture__comments');
-
-    img.src = this.url;
-    likes.textContent = this.likes;
-    comments.textContent = String(this.comments.length);
-
-    this.pictureElement.addEventListener('click', this.callRendering.bind(this));
-
-    return this.pictureElement;
-  };
-
-  Picture.prototype.remove = function () {
-    this.pictureElement.removeEventListener('click', this.callRendering);
-    picturesContainer.removeChild(this.pictureElement);
-  };
-
-  Picture.prototype.callRendering = function () {
-    window.renderFullScreenPhoto(this.data);
-  };
-
-
   var errorHandler = function (errorMessage) {
     var messageContainer = document.createElement('div');
     messageContainer.textContent = errorMessage;
@@ -83,6 +46,5 @@
 
   window.backend.getData(successHandler, errorHandler);
 
-  window.Picture = Picture;
   window.renderPictures = renderPictures;
 })();
